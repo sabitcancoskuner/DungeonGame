b6462da1850560be193e6cb506c96a14e190bd36
sabitcancoskuner <sabitcan2626@gmail.com>
2025-05-29 13:52:32 +0300

    Created animations for Soldier character.

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    [SerializeField] protected float moveSpeed;
+    protected int facingDirection = 1; // 1 for right, -1 for left
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    protected void FlipSprite()
+    {
+
+    }
+}
